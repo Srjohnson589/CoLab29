05aa12a3ab920d6cbc3ad5fadcf40fb4f33b18a9
EJacoby515 <ejacobowitz515@gmail.com>
2024-05-14 00:30:46 -0400

    first commit, initalizing app sctructure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Co.Lab Team 6 deepFocus</h1>
+      {/* Add your extension content here */}
+    </div>
+  );
+}
+
+export default App;
